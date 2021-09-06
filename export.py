49084bd5cee2cae97094771f0812ef0384f86ae3
Jordi <jpastor@clinic.cat>
2021-09-06 16:07:33 +0200

    export.py works on ubuntu20.4

--- export.py
+++ export.py
@@ -5,15 +5,16 @@ cwd = os.getcwd()
 base_script = (
     '#!/bin/bash\n'
     f'source activate {os.environ["CONDA_DEFAULT_ENV"]}\n'
-    f'python3 {os.path.join(cwd, "novullpagar.py")} "$@'
+    f'python3 {os.path.join(cwd, "novullpagar.py")} "$@"'
 )
 
 with open(os.path.join(cwd, 'novullpagar.sh'), 'w') as handle:
     handle.write(
         base_script
     )
+os.chmod('novullpagar.sh', 0o755)
 
-export_str = f'alias novullpagar="{os.path.join(cwd, "novullpagar.sh")}"'
+export_str = f'\nalias novullpagar="{os.path.join(cwd, "novullpagar.sh")}"\n'
 
 with open(os.path.expanduser("~/.bashrc"), "a") as handle:
     handle.write(export_str)
